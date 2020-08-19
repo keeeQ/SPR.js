5ca4de3b86a950cc71d5b1a330181069e4f6e967
keyhan <keyhanhq.1377@gmail.com>
2020-08-20 03:25:51 +0430

    create logger file

---
+++ s_logger.js
@@ -0,0 +1,64 @@
+//assume the log file will going to be in the __dirname path
+const {readFile} = require('fs');
+function logger(value,file,id) {
+    readFile(file,"utf8",(err,data)=>{
+        if(err) {
+            createLoggerFile((name)=>{
+                readFile(`./${name}`,"utf8",(err,data)=>{
+                    if(err) {
+                        console.log('problem while reading the file');
+                        throw err;
+                    }
+                    if(typeof data === "string" ) {
+                        let jObj = JSON.parse(data);
+                        jObj[id] = value;
+                        jObj = JSON.stringify(jObj);
+                        writeFile(name,jObj,err=>{
+                            if(err) {
+                                console.log('operation is failed');
+                                return;
+                            }
+                        });
+                    }
+                    console.log("the format is not string");
+                    return;
+                });
+            });
+            return; //shak daram be in
+            //means we dont have the file;
+
+        }
+
+        if(typeof data === "string") {
+            let jObj = JSON.parse(data);
+            jObj[id] = value;
+            jObj = JSON.stringify(jObj);
+            writeFile(file.split(".")[1],jObj,(err)=>{
+                if(err) {
+                    console.log("operation failed in the second phase of the logger");
+                    return;
+                }
+
+                console.log("logged completed");
+            });
+        }
+
+
+    });
+
+}
+
+
+function createLoggerFile(cb) {
+    writeFile('spr.log.json',"{}",err=>{
+        if(err) {
+            console.log('cant create the log file');
+            return;
+        }
+
+        console.log("log file created");
+        cb('spr.log.json');
+    });
+}
+
+
